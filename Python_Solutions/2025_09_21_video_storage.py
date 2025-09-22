9f85cadd2ef3c7d84dc0870fa3f2db9897197623
Ivan Lambrev <ilambrev@gmail.com>
2025-09-22 21:06:52 +0300

    Video Storage

--- Python_Solutions/2025_09_21_video_storage.py
+++ Python_Solutions/2025_09_21_video_storage.py
@@ -1,7 +1,7 @@
 from math import floor
 
 def number_of_videos(video_size, video_unit, drive_size, drive_unit):
-    video_units = ['KB', 'MB', 'GB']
+    video_units = ['B', 'KB', 'MB', 'GB']
     drive_units = ['GB', 'TB']
     units = {
         'B': 0,
@@ -20,7 +20,7 @@ def number_of_videos(video_size, video_unit, drive_size, drive_unit):
     return floor((drive_size * 1000 ** units[drive_unit]) / (video_size * 1000 ** units[video_unit]))
 
 # print(number_of_videos(500, "MB", 100, "GB"))
-# print(number_of_videos(2000, "B", 1, "TB"))
+# print(number_of_videos(1, "TB", 10, "TB"))
 # print(number_of_videos(2000, "MB", 100000, "MB"))
 # print(number_of_videos(500000, "KB", 2, "TB"))
 # print(number_of_videos(1.5, "GB", 2.2, "TB"))
