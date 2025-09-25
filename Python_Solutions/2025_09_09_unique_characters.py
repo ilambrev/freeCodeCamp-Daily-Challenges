9b2d2dd431ef879b0b92d384978456e9d8d6c65d
Ivan Lambrev <ilambrev@gmail.com>
2025-09-25 21:27:15 +0300

    Unique Characters

---
+++ Python_Solutions/2025_09_09_unique_characters.py
@@ -0,0 +1,11 @@
+def all_unique(s):
+
+    return len(set(s)) == len(s)
+
+# print(all_unique("abc"))
+# print(all_unique("aA"))
+# print(all_unique("QwErTy123!@"))
+# print(all_unique("~!@#$%^&*()_+"))
+# print(all_unique("hello"))
+# print(all_unique("freeCodeCamp"))
+# print(all_unique("!@#*$%^&*()aA"))
