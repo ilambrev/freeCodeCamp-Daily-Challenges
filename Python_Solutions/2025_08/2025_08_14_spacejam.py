448ff999d1abb7dee74fe4d0ca32994341338e62
Ivan Lambrev <ilambrev@gmail.com>
2025-09-26 21:46:34 +0300

    Spacejam

---
+++ Python_Solutions/2025_08/2025_08_14_spacejam.py
@@ -0,0 +1,9 @@
+def space_jam(s):
+
+    return '  '.join(s.replace(' ', '').upper())
+
+# print(space_jam("freeCodeCamp"))
+# print(space_jam("   free   Code   Camp   "))
+# print(space_jam("Hello World?!"))
+# print(space_jam("C@t$ & D0g$"))
+# print(space_jam("allyourbase"))
