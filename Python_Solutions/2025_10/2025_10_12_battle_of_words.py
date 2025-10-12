5a21f166b0d2a829230f901b8635d20b1c3973a1
Ivan Lambrev <ilambrev@gmail.com>
2025-10-12 12:53:39 +0300

    Battle Of Words

---
+++ Python_Solutions/2025_10/2025_10_12_battle_of_words.py
@@ -0,0 +1,34 @@
+from string import ascii_letters
+
+def battle(our_team, opponent):
+    our_team_words = our_team.split()
+    opponent_words = opponent.split()
+
+    our_wins = 0
+    opponent_wins = 0
+
+    for i in range(len(our_team_words)):
+        our_word_value = sum([ascii_letters.index(letter) + 1 for letter in our_team_words[i]])
+        opponent_word_value = sum([ascii_letters.index(letter) + 1 for letter in opponent_words[i]])
+
+        if our_word_value > opponent_word_value:
+            our_wins += 1
+        elif opponent_word_value > our_word_value:
+            opponent_wins += 1
+
+    result = "Draw"
+
+    if our_wins > opponent_wins:
+        result = "We win"
+    elif opponent_wins > our_wins:
+        result = "We lose"
+    
+    return result
+
+# print(battle("hello world", "hello word"))
+# print(battle("Hello world", "hello world"))
+# print(battle("lorem ipsum", "kitty ipsum"))
+# print(battle("hello world", "world hello"))
+# print(battle("git checkout", "git switch"))
+# print(battle("Cheeseburger with fries", "Cheeseburger with Fries"))
+# print(battle("We must never surrender", "Our team must win"))
