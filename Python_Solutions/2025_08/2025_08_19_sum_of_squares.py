583b7b99018d6571d2efbcdfc8d179b831ff0d24
Ivan Lambrev <ilambrev@gmail.com>
2025-10-15 10:25:09 +0300

    Sum Of Squares

---
+++ Python_Solutions/2025_08/2025_08_19_sum_of_squares.py
@@ -0,0 +1,9 @@
+def sum_of_squares(n):
+
+    return sum(number ** 2 for number in range(1, n + 1))
+
+# print(sum_of_squares(5))
+# print(sum_of_squares(10))
+# print(sum_of_squares(25))
+# print(sum_of_squares(500))
+# print(sum_of_squares(1000))
