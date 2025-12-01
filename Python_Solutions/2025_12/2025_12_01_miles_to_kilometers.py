5494ffa57939c335ce52ab6183d40fe06aeea115
Ivan Lambrev <ilambrev@gmail.com>
2025-12-01 10:24:59 +0200

    Miles to Kilometers

---
+++ Python_Solutions/2025_12/2025_12_01_miles_to_kilometers.py
@@ -0,0 +1,11 @@
+def convert_to_km(miles):
+    mile_to_kilometer = 1.60934
+    kilometers = miles * mile_to_kilometer
+
+    return round(kilometers, 2)
+
+# print(convert_to_km(1))
+# print(convert_to_km(21))
+# print(convert_to_km(3.5))
+# print(convert_to_km(0))
+# print(convert_to_km(0.621371))
