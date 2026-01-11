1998b4e05a9c1c121b5845ec2b939c17d9c6804e
Ivan Lambrev <ilambrev@gmail.com>
2026-01-11 11:08:48 +0200

    Par for the Hole

---
+++ Python_Solutions/2026_01/2026_01_11_par_for_the_hole.py
@@ -0,0 +1,20 @@
+def golf_score(par, strokes):
+    results = {
+        -2: "Eagle",
+        -1: "Birdie",
+        0: "Par",
+        1: "Bogey",
+        2: "Double bogey"
+    }
+
+    if strokes == 1:
+        return "Hole in one!"
+    else:
+        return results[strokes - par]
+
+# print(golf_score(3, 3))
+# print(golf_score(4, 3))
+# print(golf_score(3, 1))
+# print(golf_score(5, 7))
+# print(golf_score(4, 5))
+# print(golf_score(5, 3))
