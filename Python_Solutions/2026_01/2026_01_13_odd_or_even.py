8b2e95e60a07b2daf5ccce48633ad4566a952999
Ivan Lambrev <ilambrev@gmail.com>
2026-01-13 10:55:35 +0200

    Odd or Even?

---
+++ Python_Solutions/2026_01/2026_01_13_odd_or_even.py
@@ -0,0 +1,8 @@
+def odd_or_even(n):
+    return "Even" if n % 2 == 0 else "Odd"
+
+# print(odd_or_even(1))
+# print(odd_or_even(2))
+# print(odd_or_even(13))
+# print(odd_or_even(196))
+# print(odd_or_even(123456789))
