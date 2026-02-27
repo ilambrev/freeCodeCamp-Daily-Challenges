5d368af17237338812c8e865a21fd640b191c45c
Ivan Lambrev <ilambrev@gmail.com>
2026-02-27 15:09:48 +0200

    Matrix Shift

--- Python_Solutions/2026_02/2026_02_27_matrix_shift.py
+++ Python_Solutions/2026_02/2026_02_27_matrix_shift.py
@@ -12,9 +12,9 @@ def shift_matrix(matrix, shift):
 
     return [arr[i:i+row_len] for i in range(0, len(arr), row_len)]
 
-# print(shift_matrix([[1, 2, 3], [4, 5, 6]], 1))  # should return [[6, 1, 2], [3, 4, 5]]
-# print(shift_matrix([[1, 2, 3], [4, 5, 6]], -1))  # should return [[2, 3, 4], [5, 6, 1]]
-# print(shift_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 5))  # should return [[5, 6, 7], [8, 9, 1], [2, 3, 4]]
-# print(shift_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], -6))  # should return [[7, 8, 9], [1, 2, 3], [4, 5, 6]]
-# print(shift_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], 7))  # should return [[10, 11, 12, 13], [14, 15, 16, 1], [2, 3, 4, 5], [6, 7, 8, 9]]
-# print(shift_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], -54))  # should return [[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 1, 2], [3, 4, 5, 6]]
+# print(shift_matrix([[1, 2, 3], [4, 5, 6]], 1))
+# print(shift_matrix([[1, 2, 3], [4, 5, 6]], -1))
+# print(shift_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 5))
+# print(shift_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]], -6))
+# print(shift_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], 7))
+# print(shift_matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], -54))
