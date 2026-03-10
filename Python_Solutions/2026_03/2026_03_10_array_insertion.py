4e28591d751d0f50d74d70640790d88c87f1dd94
Ivan Lambrev <ilambrev@gmail.com>
2026-03-10 10:09:26 +0200

    Array Insertion

---
+++ Python_Solutions/2026_03/2026_03_10_array_insertion.py
@@ -0,0 +1,9 @@
+def insert_into_array(arr, value, index):
+    arr.insert(index, value)
+
+    return arr
+
+# print(insert_into_array([2, 4, 8, 10], 6, 2))
+# print(insert_into_array(["the", "quick", "fox"], "brown", 2))
+# print(insert_into_array([], 0, 0))
+# print(insert_into_array([0, 1, 1, 2, 3, 8, 13], 5, 5))
