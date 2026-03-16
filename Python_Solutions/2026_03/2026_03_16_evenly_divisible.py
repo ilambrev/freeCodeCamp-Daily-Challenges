4e472fd0a532d2e804106e098d2d9d1c8c865f6c
Ivan Lambrev <ilambrev@gmail.com>
2026-03-16 09:38:35 +0200

    Evenly Divisible

---
+++ Python_Solutions/2026_03/2026_03_16_evenly_divisible.py
@@ -0,0 +1,10 @@
+def is_evenly_divisible(a, b):
+
+    return a % b == 0
+
+# print(is_evenly_divisible(4, 2))
+# print(is_evenly_divisible(7, 3))
+# print(is_evenly_divisible(5, 10))
+# print(is_evenly_divisible(48, 6))
+# print(is_evenly_divisible(3186, 9))
+# print(is_evenly_divisible(4192, 11))
