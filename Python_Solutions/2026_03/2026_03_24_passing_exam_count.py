c0a229e414344d9e734286648d8e92db94adcf67
Ivan Lambrev <ilambrev@gmail.com>
2026-03-24 09:36:58 +0200

    Passing Exam Count

---
+++ Python_Solutions/2026_03/2026_03_24_passing_exam_count.py
@@ -0,0 +1,9 @@
+def passing_count(scores, passing_score):
+
+    return len([s for s in scores if s >= passing_score])
+
+# print(passing_count([90, 85, 75, 60, 50], 70))
+# print(passing_count([100, 80, 75, 88, 72, 74, 79, 71, 60, 92], 75))
+# print(passing_count([79, 60, 88, 72, 74, 59, 75, 71, 80, 92], 60))
+# print(passing_count([76, 79, 80, 70, 71, 65, 79, 78, 59, 72], 85))
+# print(passing_count([84, 65, 98, 53, 58, 71, 91, 80, 92, 70, 73, 83, 86, 69, 84, 77, 72, 58, 69, 75, 66, 68, 72, 96, 90, 63, 88, 63, 80, 67], 60))
