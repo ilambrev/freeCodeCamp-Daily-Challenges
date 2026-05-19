46d63970f51eeb5b76c9df30fa2a470a65b3559a
Ivan Lambrev <ilambrev@gmail.com>
2026-05-19 09:08:00 +0300

    Sleep Debt

--- Python_Solutions/2026_05/2026_05_19_sleep_debt.py
+++ Python_Solutions/2026_05/2026_05_19_sleep_debt.py
@@ -3,8 +3,8 @@ def sleep_debt(hours_slept, target_hours):
 
     return dept if dept > 0 else 0
 
-# print(sleep_debt([6, 6, 6, 6, 6, 6], 8))  # should return 20
-# print(sleep_debt([6, 7, 8, 4, 8, 6], 7))  # should return 10
-# print(sleep_debt([10, 10, 9, 10, 9, 11], 9))  # should return 4
-# print(sleep_debt([8, 7, 6, 7, 6, 8], 6))  # should return 0
-# print(sleep_debt([8, 9, 10, 9, 10, 7], 7))  # should return 0
+# print(sleep_debt([6, 6, 6, 6, 6, 6], 8))
+# print(sleep_debt([6, 7, 8, 4, 8, 6], 7))
+# print(sleep_debt([10, 10, 9, 10, 9, 11], 9))
+# print(sleep_debt([8, 7, 6, 7, 6, 8], 6))
+# print(sleep_debt([8, 9, 10, 9, 10, 7], 7))
