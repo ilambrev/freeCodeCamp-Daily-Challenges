2f70ef18ed91f06a3892b9d05bd6f2e03ed7a50f
Ivan Lambrev <ilambrev@gmail.com>
2026-05-25 10:13:16 +0300

    Secret Number

---
+++ Python_Solutions/2026_05/2026_05_25_secret_number.py
@@ -0,0 +1,10 @@
+def guess_number(secret, guess):
+
+    return "higher" if guess < secret else "lower" if guess > secret else "you got it!"
+
+# print(guess_number(50, 30))
+# print(guess_number(85, 99))
+# print(guess_number(2026, 2026))
+# print(guess_number(92904, 11283))
+# print(guess_number(230495, 423920))
+# print(guess_number(120349, 120349))
