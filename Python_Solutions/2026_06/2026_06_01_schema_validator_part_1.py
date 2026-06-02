34cbef18372e02786f47732e1cc851d2c0405a91
Ivan Lambrev <ilambrev@gmail.com>
2026-06-02 18:09:56 +0300

    Schema Validator Part 1

--- Python_Solutions/2026_06/2026_06_01_schema_validator_part_1.py
+++ Python_Solutions/2026_06/2026_06_01_schema_validator_part_1.py
@@ -1,7 +1,7 @@
 def is_valid_schema(obj):
     k, v = next(iter(obj.items()))
 
-    return isinstance(k, str) and isinstance(v, str)
+    return k == "username" and isinstance(v, str)
 
 # print(is_valid_schema({"username": "bob"}))
 # print(is_valid_schema({"username": "jen", "posts": 30}))
