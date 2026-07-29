6d0d8c96391f2e8deb9fd64f0c07c271c1ddf836
Ivan Lambrev <ilambrev@gmail.com>
2026-07-29 18:11:33 +0300

    Elevator Stops

--- Python_Solutions/2026_07/2026_07_19_elevator_stops.py
+++ Python_Solutions/2026_07/2026_07_19_elevator_stops.py
@@ -10,8 +10,8 @@ def elevator_stops(current_floor, stops):
 
     return order
 
-# print(elevator_stops(5, [2, 8, 3, 9]))  # should return [3, 2, 8, 9]
-# print(elevator_stops(6, [2, 10, 8, 3, 1, 9]))  #should return [8, 9, 10, 3, 2, 1]
-# print(elevator_stops(1, [4, 8, 3, 6, 9]))  # should return [3, 4, 6, 8, 9]
-# print(elevator_stops(12, [6, 10, 7, 3, 1, 4]))  # should return [10, 7, 6, 4, 3, 1]
-# print(elevator_stops(11, [2, 8, 23, 5, 12, 10, 6, 9, 19]))  # should return [10, 9, 8, 6, 5, 2, 12, 19, 23]
+# print(elevator_stops(5, [2, 8, 3, 9]))
+# print(elevator_stops(6, [2, 10, 8, 3, 1, 9]))
+# print(elevator_stops(1, [4, 8, 3, 6, 9]))
+# print(elevator_stops(12, [6, 10, 7, 3, 1, 4]))
+# print(elevator_stops(11, [2, 8, 23, 5, 12, 10, 6, 9, 19]))
